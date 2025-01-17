7af9bf67747c73ec315e909c531253adf8260f88
eboboeing-747 <silinskiymihail@gmail.com>
2025-01-17 04:06:38 +0300

    parse.c, parse.h added
    parse.c is not fully tested

---
+++ src/parse.c
@@ -0,0 +1,268 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#include "parse.h"
+
+// debug libs
+#include <stdio.h>
+
+bool is_scheme(const char* str)
+{
+    const char* i = str;
+    while (*i && isalpha(*i))
+        i++;
+
+    return (i > str) && (strncmp(i, "://", 3) == 0);
+}
+
+size_t count_delims(const char* str, const char* delimeter)
+{
+    size_t amount = 0;
+    size_t delim_size = strlen(delimeter);
+
+    for (const char* i = str; (i = strstr(i, delimeter)); i += delim_size)
+        amount++;
+
+    return amount;
+}
+
+char* split_next(char* str, const char* delimeter)
+{
+    if (!*str)
+        return NULL;
+
+    char* end = strstr(str, delimeter);
+    
+    if (end == NULL)
+        return NULL;
+
+    *end = '\0';
+
+    return end + strlen(delimeter);
+}
+
+struct str_arr split(char* str, const char* delimeter)
+{
+    struct str_arr tokens;
+    tokens.size = count_delims(str, delimeter) + 1;
+    tokens.array = (char**)malloc(sizeof(char*) * tokens.size);
+
+    for (size_t i = 0; str != NULL; str = split_next(str, delimeter), i++)
+        tokens.array[i] = str;
+
+    return tokens;
+}
+
+char* split_request(char* request)
+{
+    const char* request_delimeter = "\r\n\r\n";
+    char* message_body = strstr(request, request_delimeter);
+
+    if (message_body == NULL)
+        return NULL;
+
+    *message_body = '\0';
+    message_body += strlen(request_delimeter);
+
+    return message_body;
+}
+
+enum status_code validate_http_method(char* method)
+{
+    for (int i = 0; i < HTTP_METHOD_AMOUNT; i++)
+    {
+        if (strcmp(method, HTTP_METHODS[i]) == 0)
+            return VALID;
+    }
+
+    return CE_400;
+}
+
+struct request_uri validate_http_uri(char* uri_str, char* method, enum status_code* status)
+{
+    struct request_uri uri;
+    uri.uri = uri_str;
+    *status = VALID;
+    
+    if (!*uri_str)
+    {
+        *status = CE_400; // request-URI can never be empty
+        uri.type = INVALID;
+        return uri;
+    }
+
+    if (*uri_str == '*')
+    {
+        if (strcmp(method, "OPTIONS") == 0)
+        {
+            uri.type = asterisk;
+            return uri;
+        }
+        else
+        {
+            *status = CE_400; // the only method campatible with request-URI = "*" is OPTIONS
+            uri.type = asterisk;
+            return uri;
+        }
+    }
+
+    if (is_scheme(uri_str))
+    {
+        uri.type = absoluteURI; // if request-URI starts with scheme
+        return uri;
+    }
+
+    if (*uri_str == '/')
+    {
+        uri.type = abs_path; // if request-URI starts with "/"
+        return uri;
+    }
+
+    uri.type = authority;
+    return uri;
+}
+
+int vercmp(const struct http_version* lho, const struct http_version* rho)
+{
+    if (lho->major < rho->major)
+        return -1;
+    else if (lho->major > rho->major)
+        return 1;
+
+    if (lho->minor < rho->minor)
+        return -1;
+    else if (lho->minor > rho->minor)
+        return 1;
+    else
+        return 0;
+}
+
+bool isnumber(const char* number)
+{
+    if (!*number || number == NULL)
+        return false;
+
+    for (; *number; number++)
+    {
+        if (!isdigit(*number))
+            return false;
+    }
+
+    return true;
+}
+
+struct http_version parse_http_version(char* version_str, enum status_code* status)
+{
+    struct http_version version;
+    *status = CE_400;
+
+    char* major_str = split_next(version_str, "/");
+    if (major_str == NULL)
+        return version;
+
+    char* minor_str = split_next(major_str, ".");
+    if (minor_str == NULL)
+        return version;
+
+    if (!isnumber(major_str) || !isnumber(minor_str))
+        return version;
+
+    version.major = atoi(major_str);
+    version.minor = atoi(minor_str);
+    *status = VALID;
+    return version;
+}
+
+enum status_code parse_request(char* request)
+{
+    char* message_body = split_request(request);
+
+    if (message_body == NULL)
+        return CE_400; // 400 Bad Request (failed to find '\r\n\r\n' in request)
+    
+    struct str_arr tokens = split(request, "\r\n");
+
+    if (tokens.size < 1)
+    {
+        free(tokens.array);
+        return CE_400; // 400 Bad Request (no header fields)
+    }
+
+    // parse request line
+    char* request_line_str = tokens.array[0];
+    struct str_arr request_line_tokens = split(request_line_str, " ");
+    
+    if (request_line_tokens.size != REQ_LINE_SIZE)
+    {
+        free(request_line_tokens.array);
+        free(tokens.array);
+        return CE_400; // 400 Bad Request (invalid request line length)
+    }
+
+    struct request_line req_line;
+    enum status_code status;
+
+    status = validate_http_method(request_line_tokens.array[0]);
+    if (status != VALID)
+    {
+        free(request_line_tokens.array);
+        free(tokens.array);
+        return status;
+    }
+    req_line.method = request_line_tokens.array[0];
+
+    struct request_uri uri = validate_http_uri(request_line_tokens.array[1], req_line.method, &status);
+    if (status != VALID)
+    {
+        free(request_line_tokens.array);
+        free(tokens.array);
+        return status;
+    }
+    req_line.uri = uri;
+
+    struct http_version version = parse_http_version(request_line_tokens.array[2], &status);
+    if (status != VALID)
+    {
+        free(request_line_tokens.array);
+        free(tokens.array);
+        return status;
+    }
+    req_line.version = version;
+
+    free(request_line_tokens.array);
+    // end parse request line
+
+    printf("method: '%s'\nrequest-URI: '%s', type: '%d'\nHTTP-version/'%d'.'%d'\n\n", req_line.method, req_line.uri.uri, req_line.uri.type, req_line.version.major, req_line.version.minor);
+
+    free(tokens.array);
+    return VALID;
+}
+
+int main()
+{
+    char request[] = "GET /style.css HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0\r\nAccept: text/css,*/*;q=0.1\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate, br, zstd\r\nConnection: keep-alive\r\nReferer: http://localhost:8080/\r\nSec-Fetch-Dest: style\r\nSec-Fetch-Mode: no-cors\r\nSec-Fetch-Site: same-origin\r\nPriority: u=2\r\n\r\n";
+
+    enum status_code status = parse_request(request);
+    printf("%d", status);
+
+    /*char* message_body = split_request(request);
+
+    if (message_body == NULL)
+    {
+        printf("no request delimeter\nbad request");
+        return 0;
+    }
+
+    printf("'%s'\n\n'%s'\n", request, message_body);
+
+    struct str_arr tokens = split(request, "\r\n");
+
+    for (int i = 0; i < tokens.size; i++)
+        printf("%d: '%s'\n", i, tokens.array[i]);
+
+    free(tokens.array);*/
+
+    return 0;
+}
---
+++ src/parse.h
@@ -0,0 +1,108 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct str_arr
+{
+    char** array;
+    size_t size;
+};
+
+#define HTTP_METHOD_AMOUNT 8
+const char* HTTP_METHODS[HTTP_METHOD_AMOUNT] = { "OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT" };
+
+enum status_code
+{
+    I_100 = 0,
+    I_101 = 1,
+    S_200 = 2,
+    S_201 = 3,
+    S_202 = 4,
+    S_203 = 5,
+    S_204 = 6,
+    S_205 = 7,
+    S_206 = 8,
+    R_300 = 9,
+    R_301 = 10,
+    R_302 = 11,
+    R_303 = 12,
+    R_304 = 13,
+    R_305 = 14,
+    R_306 = 15,
+    R_307 = 16,
+    CE_400 = 17,
+    CE_401 = 18,
+    CE_402 = 19,
+    CE_403 = 20,
+    CE_404 = 21,
+    CE_405 = 22,
+    CE_406 = 23,
+    CE_407 = 24,
+    CE_408 = 25,
+    CE_409 = 26,
+    CE_410 = 27,
+    CE_411 = 28,
+    CE_412 = 29,
+    CE_413 = 30,
+    CE_414 = 31,
+    CE_415 = 32,
+    CE_416 = 33,
+    CE_417 = 34,
+    SE_500 = 35,
+    SE_501 = 36,
+    SE_502 = 37,
+    SE_503 = 38,
+    SE_504 = 39,
+    SE_505 = 40,
+    VALID = 41
+};
+
+enum uri_type
+{
+    asterisk = 0,
+    absoluteURI = 1,
+    abs_path = 2,
+    authority = 3,
+    INVALID = 4
+};
+
+struct request_uri
+{
+    char* uri;
+    enum uri_type type;
+};
+
+#define HTTP_MAJOR 1
+#define HTTP_MINOR 1
+struct http_version
+{
+    int major;
+    int minor;
+};
+
+int vercmp(const struct http_version* lho, const struct http_version* rho);
+// returns: -1 if lho < rho, 1 if lho > rho, 0 if lho == rho
+
+enum status_code validate_http_method(char* method);
+// returns: CE_400 if it fails to recognize method
+
+struct request_uri validate_http_uri(char* uri_str, char* method, enum status_code* status);
+// makes status be: CE_400 if it
+
+struct http_version parse_http_version(char* version, enum status_code* status);
+// makes status be: CE_400 if it fails to recognize version as http-version, SE_505 if http-version not supported, VALID otherwise
+
+#define REQ_LINE_SIZE 3
+struct request_line
+{
+    char* method;
+    struct request_uri uri;
+    struct http_version version;
+};
+
+char* split_request(char* request);
+// returns char* to message-body, makes "\r\n\r\n" -> "\0\n\r\n"
+
+#endif // PARSE_H
